453bd7845bde6d66e0313adb5fad91d916631af8
Jay Hodgson <jay.hodgson@sagebase.org>
2018-08-29 15:34:23 -0700

    SWC-4363: redirect to my profile if visiting registration page while logged in

--- src/test/java/org/sagebionetworks/web/unitclient/presenter/RegisterAccountPresenterTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/presenter/RegisterAccountPresenterTest.java
@@ -1,20 +1,29 @@
 package org.sagebionetworks.web.unitclient.presenter;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.web.client.GlobalApplicationState;
+import org.sagebionetworks.web.client.PlaceChanger;
+import org.sagebionetworks.web.client.place.Profile;
 import org.sagebionetworks.web.client.place.users.RegisterAccount;
 import org.sagebionetworks.web.client.presenter.users.RegisterAccountPresenter;
+import org.sagebionetworks.web.client.security.AuthenticationController;
 import org.sagebionetworks.web.client.view.users.RegisterAccountView;
 import org.sagebionetworks.web.client.view.users.RegisterWidget;
 import org.sagebionetworks.web.client.widget.header.Header;
 
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.place.shared.Place;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -31,16 +40,24 @@ public class RegisterAccountPresenterTest {
 	RegisterWidget mockRegisterWidget;
 	@Mock
 	Header mockHeader;
-
+	@Mock
+	AuthenticationController mockAuthController;
+	@Mock
+	GlobalApplicationState mockGlobalAppState;
 	@Mock
 	EventBus mockEventBus;
 	@Mock
+	PlaceChanger mockPlaceChanger;
+	@Mock
 	AcceptsOneWidget mockAcceptsOneWidget;
-	
+	@Captor
+	ArgumentCaptor<Place> placeCaptor;
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
-		registerAccountPresenter = new RegisterAccountPresenter(mockView, mockRegisterWidget, mockHeader);			
+		when(mockAuthController.isLoggedIn()).thenReturn(false);
+		when(mockGlobalAppState.getPlaceChanger()).thenReturn(mockPlaceChanger);
+		registerAccountPresenter = new RegisterAccountPresenter(mockView, mockRegisterWidget, mockHeader, mockAuthController, mockGlobalAppState);
 	}
 	
 	@Test
@@ -59,5 +76,17 @@ public class RegisterAccountPresenterTest {
 		verify(mockView).setRegisterWidget(any(Widget.class));
 		verify(mockHeader).configure(false);
 		verify(mockHeader).refresh();
+	}
+	
+	@Test
+	public void testSetPlaceLoggedIn() {
+		when(mockAuthController.isLoggedIn()).thenReturn(true);
+		
+		registerAccountPresenter.setPlace(mockPlace);
+		
+		verify(mockPlaceChanger).goTo(placeCaptor.capture());
+		Place wentToPlace = placeCaptor.getValue();
+		assertTrue(wentToPlace instanceof Profile);
+		assertEquals(Profile.VIEW_PROFILE_TOKEN, ((Profile)wentToPlace).toToken());
 	}
 }
